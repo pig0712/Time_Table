c218552878cca51678a3b234b4623e7955b5621b
pig0712 <koll20040712@gmail.com>
2023-11-10 10:51:41 +0900

    WinUGI 파일 수정 -배진우-

---
+++ Generator.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class widget:
+    def ButtonWidget(self):
+        pass
+
+    def EnterWidget(self):
+        pass
+
+    def LabelWidget(self):
+        pass
--- winGUI.py
+++ winGUI.py
@@ -1,26 +1,24 @@
 import tkinter as tk
 
-class WindowGenerator:
-    def Normal(self,name : str = "Unknown", x : int = 1000, y : int = 750, xrs : bool = True, yrs : bool = True):
-        window = tk.Tk()
-        window.title(name)
-        window.geometry(f"{x}x{y}")
-        window.resizable(xrs, yrs)
-        window.mainloop()
+class WindowGen:
+    def __init__(self):
+        self.window = tk.Tk()
 
-    def Toplvl(self,name : str = "Unknown", x : int = 1000, y : int = 750, xrs : bool = True, yrs : bool = True):
-        window = tk.Toplevel()
-        window.title(name)
-        window.geometry(f"{x}x{y}")
-        window.resizable(xrs, yrs)
-        window.mainloop()
+    def normal(self, name="Unknown", x=1000, y=750, xrs=True, yrs=True):
+        self.window.title(name)
+        self.window.geometry(f"{x}x{y}")
+        self.window.resizable(xrs, yrs)
+
+    def toplvl(self, name="Unknown", x=1000, y=750, xrs=True, yrs=True):
+        self.window = tk.Toplevel(name)
+        self.window.title(name)
+        self.window.geometry(f"{x}x{y}")
+        self.window.resizable(xrs, yrs)
+
+    def run(self):
+        self.window.mainloop()
 
 if __name__ == "__main__":
-    window_gen = WindowGenerator()
-    window_gen.Normal(name="test", x=800, y=600, xrs=False, yrs=False)
-    # 인자 설명
-    # 1. 윈도우 창 이름
-    # 2. 윈도우 x 축 길이
-    # 3. 윈도우 y 축 길이
-    # 4. 윈도우 x 축 길이 변환 가능 여부
-    # 5. 윈도우 y 축 길이 변환 가능 여부
+    window_gen = WindowGen()
+    window_gen.normal(name="test", x=800, y=600, xrs=False, yrs=False)
+    window_gen.run()
